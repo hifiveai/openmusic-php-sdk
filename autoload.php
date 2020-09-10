980f8ad82a2e8109cb952f1107f3600b09d2c733
zhaowei <zhaowei@ilongyuan.com.cn>
2020-09-10 14:34:24 +0800

    自动加载路径

--- autoload.php
+++ autoload.php
@@ -1,28 +1,22 @@
 <?php
 
-spl_autoload_register('Autoloader::autoload');
-
-class Autoloader {
-
-    private static $autoloadPathArray = array(
+spl_autoload_register(function ($class) {
+    $root = dirname(__FILE__);
+    $dirs = array(
         'src',
-        'src/Auth',
-        'src/Exception',
-        'src/Http',
-        'src/Profile',
-        'src/Request',
-        'src/Util',
+        'src\Auth',
+        'src\Exception',
+        'src\Http',
+        'src\Profile',
+        'src\Request',
+        'src\Util',
     );
 
-    public static function autoload($className) {
-        $root = dirname(dirname(__DIR__));
-        foreach (self::$autoloadPathArray as $map) {
-            $file = $root . DIRECTORY_SEPARATOR . $map . DIRECTORY_SEPARATOR . $className . '.php';
-            if (is_file($file)) {
-                include_once $file;
-                break;
-            }
+    foreach ($dirs as $dir) {
+        $path = $root . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $class . '.php';
+        if (file_exists($path)) {
+            include_once($path);
+            return;
         }
     }
-
-}
+});
