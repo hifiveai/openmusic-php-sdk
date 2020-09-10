235b84a27f6074c6fec23330420bd6c6ab043360
zhaowei <zhaowei@ilongyuan.com.cn>
2020-09-10 14:15:16 +0800

    调整目录

--- autoload.php
+++ autoload.php
@@ -5,13 +5,13 @@ spl_autoload_register('Autoloader::autoload');
 class Autoloader {
 
     private static $autoloadPathArray = array(
-        'HIFIVE/OpenApi/src',
-        'HIFIVE/OpenApi/src/Auth',
-        'HIFIVE/OpenApi/src/Exception',
-        'HIFIVE/OpenApi/src/Http',
-        'HIFIVE/OpenApi/src/Profile',
-        'HIFIVE/OpenApi/src/Request',
-        'HIFIVE/OpenApi/src/Util',
+        'src',
+        'src/Auth',
+        'src/Exception',
+        'src/Http',
+        'src/Profile',
+        'src/Request',
+        'src/Util',
     );
 
     public static function autoload($className) {
--- test/SampleTest.php
+++ test/SampleTest.php
@@ -4,6 +4,6 @@ header('Content-Type: text/html; charset=utf-8');
 include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '../src/Config.php';
 include_once 'Sample.php';
 
-$client = new Sample('https://hifive-openapi-qa.hifiveai.com', '5216d02806d5464b943492838b7e4390', '2d241e8f934d47d5');
+$client = new Sample('https://hifive-openapi-qa.hifiveai.com', 'your app id', 'your app secret');
 $res = $client->hqlListenTest();
 print_r($res);
