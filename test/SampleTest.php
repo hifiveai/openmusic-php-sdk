5d548e3a64311ad346e23c6895d1240cfd9c9ecc
zhaowei <zhaowei@ilongyuan.com.cn>
2020-09-10 14:41:39 +0800

    测试

--- test/SampleTest.php
+++ test/SampleTest.php
@@ -4,6 +4,6 @@ header('Content-Type: text/html; charset=utf-8');
 include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '../src/Config.php';
 include_once 'Sample.php';
 
-$client = new Sample('https://hifive-openapi-qa.hifiveai.com', 'your app id', 'your app secret');
+$client = new Sample('https://hifive-openapi-qa.hifiveai.com', 'your_app_id', 'your_app_secret');
 $res = $client->hqlListenTest();
 print_r($res);
