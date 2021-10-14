8b620421f218492fe4bf5ddfbfd862c0b7c3f7e5
梅阳 <meiyang@hifive.ai>
2021-10-14 19:03:45 +0800

    添加热歌与新歌

---
+++ src/Request/HFProfessionalHotRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+class HFProfessionalHotRequest extends HFRequest {
+
+    public $page;
+    public $pageSize;
+
+    function __construct() {
+        $this->actionName('ProfessionalHot')
+            ->param(array('page',
+                'pageSize'));
+    }
+
+    public function page($page) {
+        $this->page = $page;
+        return $this;
+    }
+
+    public function pageSize($size) {
+        $this->pageSize = $size;
+        return $this;
+    }
+
+}
