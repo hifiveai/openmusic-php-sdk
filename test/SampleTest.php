4586cabb476e5ff89dcbfd7e94f979c7af460b3d
zhaowei <ireturned@aliyun.com>
2021-10-14 19:16:33 +0800

    add test

--- test/SampleTest.php
+++ test/SampleTest.php
@@ -4,8 +4,10 @@ header('Content-Type: text/html; charset=utf-8');
 include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '../src/Config.php';
 include_once 'Sample.php';
 
-$client = new Sample('https://gateway.open.hifiveai.com', '3faeec81030444e98acf6af9ba32752a', '59b1aff189b3474398');
-$client->baseHotTest();
+$client = new Sample('https://hifive-gateway-pre.hifiveai.com', '3faeec81030444e98acf6af9ba32752a', '59b1aff189b3474398');
+$client->professionalHotTest();
+$client->professionalNewTest();
+
 //$method = get_class_methods($client);
 //foreach ($method as $map) {
 //    if ('__construct' == $map) {
