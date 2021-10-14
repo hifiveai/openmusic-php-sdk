faaa0ce67aa17bf09e4f7550860d7dc6d61e50ee
梅阳 <meiyang@hifive.ai>
2021-10-14 21:48:55 +0800

    更新

--- src/Request/HFRequest.php
+++ src/Request/HFRequest.php
@@ -10,7 +10,7 @@ abstract class HFRequest {
     private $acceptFormat = 'JSON';
     private $actionName;
     private $method = 'GET';
-    private $version = 'V4.1.1';
+    private $version = 'V4.1.2';
     private $param = array();
 
     public function getClientId() {
