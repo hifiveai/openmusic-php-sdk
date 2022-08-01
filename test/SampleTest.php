ce512c125621e8cb38f62ef7f65ec762ead6252b
梅阳 <meiyang@hifive.ai>
2022-08-01 10:08:11 +0800

    更新

--- test/SampleTest.php
+++ test/SampleTest.php
@@ -5,8 +5,8 @@ include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '../src/Config.php';
 include_once 'Sample.php';
 
 $client = new Sample('https://gateway.open.hifiveai.com', '3faeec81030444e98acf6af9ba32752a', '59b1aff189b3474398');
-$client->sheetTest();
-
+//$client->sheetTest();
+$client->HFMusicSkuRequest();
 
 
 //$method = get_class_methods($client);
